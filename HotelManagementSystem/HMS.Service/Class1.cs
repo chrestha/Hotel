6701c18aa62898092fe88b873a25512fa509f66d
ANIX\Anish <ash@codebee.dk>
2017-03-01 15:34:42 +0545

    project File addded

---
+++ HotelManagementSystem/HMS.Service/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Service
+{
+    public class Class1
+    {
+    }
+}
